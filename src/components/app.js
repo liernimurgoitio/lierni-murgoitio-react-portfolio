3f88ca1272ecee55b691c648bf8a6ddabd611df4
LiMur <murgoitiolierni@gmail.com>
2023-06-14 18:05:32 +0200

    actualizacion

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
-import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import PortfolioManager from './pages/portfolio-manager';
+import PortfolioDetail from './portfolio/portfolio-detail';
+import Auth from './pages/auth';
+import NoMatch from './pages/no-match';
+
 
 export default class App extends Component {
   render() {
