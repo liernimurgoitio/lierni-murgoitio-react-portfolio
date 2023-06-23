80089b01a5668491c966954849c8e4c79cc64505
LiMur <murgoitiolierni@gmail.com>
2023-06-23 11:04:58 +0200

    actualizacion 23/06/2023

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -9,47 +9,67 @@ export default class PortfolioManager extends Component {
     super();
 
     this.state = {
-      portfolioItems: []
+      portfolioItems: [],
+      portfolioToEdit: {}
     };
 
-    this.handleSuccesfulFormSubmission = this.handleSuccesfulFormSubmission.bind(this);
+    this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
+    this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this);
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.clearPortfolioToEdit = this.clearPortfolioToEdit.bind(this);
+  }
+
+  clearPortfolioToEdit() {
+    this.setState({
+      portfolioToEdit: {}
+    });
+  }
+
+  handleEditClick(portfolioItem) {
+    this.setState({
+      portfolioToEdit: portfolioItem
+    });
   }
 
   handleDeleteClick(portfolioItem) {
     axios
       .delete(
-        `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`, 
-        {withCredentials: true}
-        )
-        .then(response => {
-          this.setState({
-            portfoliItems: this.state.portfolioItems.filter(item => {
-              return item.id !== portfolioItem.id;
-            })
+        `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          portfolioItems: this.state.portfolioItems.filter(item => {
+            return item.id !== portfolioItem.id;
           })
-
-          return response.data;
-        })
-        .catch(error => {
-          console.log("handleDeleteClick error", error);
         });
+
+        return response.data;
+      })
+      .catch(error => {
+        console.log("handleDeleteClick error", error);
+      });
   }
 
-  handleSuccesfulFormSubmission(portfolioItem) {
+  handleEditFormSubmission() {
+    this.getPortfolioItems();
+  }
+
+  handleNewFormSubmission(portfolioItem) {
     this.setState({
       portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
     });
   }
 
   handleFormSubmissionError(error) {
-    console.log("handleFormSubmissionError erro", error);
+    console.log("handleFormSubmissionError error", error);
   }
- 
+
   getPortfolioItems() {
     axios
-    .get("https://liernimurgoitio.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", {
+      .get("https://liernimurgoitio.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", {
       withCredentials: true
     })
     .then(response => {
@@ -70,16 +90,20 @@ render() {
   return (
     <div className="portfolio-manager-wrapper">
       <div className="left-column">
-        <PortfolioForm 
-          handleSuccesfulFormSubmission={this.handleSuccesfulFormSubmission}
+        <PortfolioForm
+          handleNewFormSubmission={this.handleNewFormSubmission}
+          handleEditFormSubmission={this.handleEditFormSubmission}
           handleFormSubmissionError={this.handleFormSubmissionError}
+          clearPortfolioToEdit={this.clearPortfolioToEdit}
+          portfolioToEdit={this.state.portfolioToEdit}
         />
       </div>
 
       <div className="right-column">
-        <PortfolioSidebarList 
-        handleDeleteClick={this.handleDeleteClick}
-        data={this.state.portfolioItems} 
+        <PortfolioSidebarList
+          handleDeleteClick={this.handleDeleteClick}
+          data={this.state.portfolioItems}
+          handleEditClick={this.handleEditClick}
         />
       </div>
     </div>
